680c587de19d466268b7fb6000a29734ba4e5905
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2024-07-09 20:18:57 -0600

    change in url image profile

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <section className="text-white py-1 bg-Dark-Grey md:w-5/12 w-10/12  flex flex-col font-inter items-center justify-center h-5/6 rounded-md">
       <div className="w-5/6 h-1/3 flex flex-col  items-center justify-center">
         <img 
-          src="../public/profile-david.jpg" 
+          src="/profile-david.jpg" 
           className="h-[60%] md:w-[26%]  w-1/3 rounded-[50%]"/>
         <div className="flex flex-col justify-center items-center">
           <h1 className=" text-lg font-semibold">David Gutierrez</h1>
