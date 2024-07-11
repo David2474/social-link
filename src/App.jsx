55f7600947b712785b7c83f199e92cd16b841abb
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2024-07-10 20:48:53 -0600

    add new changes in desing

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
       <div className="w-5/6 h-1/3 flex flex-col  items-center justify-center">
         <img 
           src="/profile-david.jpg" 
-          className="h-[60%] md:w-[26%]  w-1/3 rounded-[50%]"/>
+          className="h-[60%] md:w-[30%]  w-[45%] rounded-[50%]"/>
         <div className="flex flex-col justify-center items-center">
           <h1 className=" text-lg font-semibold">David Gutierrez</h1>
           <p className="text-Green text-xs">Developer in progress</p>
         </div>
       </div>
 
-      <div className="flex flex-col items-center pb-1 justify-around h-4/6 w-5/6">
+      <div className="flex flex-col items-center p-2 justify-evenly h-4/6 w-5/6">
         <UsersLinks/>
       </div>
     </section>
