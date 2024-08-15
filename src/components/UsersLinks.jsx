3308920ca38f2ac9a798a20a15d87c6047447a87
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2024-08-15 10:34:40 -0600

    add changes in tag Userlink

--- src/components/UsersLinks.jsx
+++ src/components/UsersLinks.jsx
@@ -6,27 +6,32 @@ export default function UsersLinks() {
     {
       id: 1,
       title: 'Instagram',
-      link: 'https://www.instagram.com/deivid24745/'
+      link: 'https://www.instagram.com/deivid24745/',
+      img: 'icons/instagram.svg',
     },
     {
       id: 2,
       title: 'Tiktok',
-      link: 'https://www.tiktok.com/@david24745?lang=es'
+      link: 'https://www.tiktok.com/@david24745?lang=es',
+      img: 'icons/tiktok.svg',
     },
     {
       id: 3,
       title: 'Github',
-      link:'https://github.com/David2474'
+      link:'https://github.com/David2474',
+      img: 'icons/github.svg',
     },
     {
       id: 4,
       title: 'Linkln',
-      link:'https://www.linkedin.com/in/dev-david-gutierrez/'
+      link:'https://www.linkedin.com/in/dev-david-gutierrez/',
+      img: 'icons/linkedin.svg',
     },
     {
       id: 5,
       title: 'Portfolio',
-      link:'https://davidgutierrez5.netlify.app/'
+      link:'https://davidgutierrez5.netlify.app/',
+      img: 'icons/user.svg',
     }
   ] 
 
@@ -34,11 +39,15 @@ export default function UsersLinks() {
     <>
       {links.map((link) =>{
         return(
-          <p 
+          <a 
             key={link.id}
-            className="bg-Grey hover:bg-zinc-900 md:p-2 p-[10px]  md:w-11/12 w-5/6 md:text-base text-lg rounded-md flex flex-col justify-center items-center">
-            <a href={link.link}>{link.title}</a>
-          </p>
+            href={link.link}
+            className="bg-Grey hover:bg-zinc-900 md:p-2 p-[10px]  md:w-11/12 w-5/6 md:text-base text-lg rounded-md flex justify-center items-center">
+            
+            <img src={link.img} alt="image icon" />
+            <p className="mx-2">{link.title}</p>
+
+          </a>
         )
       })}
     </>
